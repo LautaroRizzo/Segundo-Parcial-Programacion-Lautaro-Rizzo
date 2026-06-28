08e707c0fecdf76090e84cfcc02625675f6c67b7
Lautaro Rizzo <lautarorizzo123@gmail.com>
2026-06-28 20:33:37 -0300

    modulo

---
+++ estadistica.py
@@ -0,0 +1,41 @@
+from archivos import cargar_datos
+
+def calcular_estadisticas():
+    lista = cargar_datos()
+    total = len(lista)
+    
+    if total == 0:
+        return {
+            "total": 0,
+            "promedio": 0,
+            "mayor_nota": None,
+            "aprobados": 0,
+            "desaprobados": 0
+        }
+    
+    suma_notas = 0
+    mayor = -1
+    alumno_mayor = None
+    aprobados = 0
+    desaprobados = 0
+    
+    for alumno in lista:
+        nota = alumno["nota"]
+        suma_notas = suma_notas + nota
+        
+        if nota > mayor:
+            mayor = nota
+            alumno_mayor = alumno
+        
+        if nota >= 6:
+            aprobados = aprobados + 1
+        else:
+            desaprobados = desaprobados + 1
+    
+    return {
+        "total": total,
+        "promedio": round(suma_notas / total, 2),
+        "mayor_nota": alumno_mayor,
+        "aprobados": aprobados,
+        "desaprobados": desaprobados
+    }
